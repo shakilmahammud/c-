42768e33dedb22319d19385830951d8ec281ee67
shakilmahammud <hshakil987@gmail.com>
2021-12-29 23:59:03 +0600

    inter stack

---
+++ programming-Hero/stack/stack.js
@@ -0,0 +1,14 @@
+const stackdata = []
+
+console.log(stackdata )
+ stackdata.push(2)
+ stackdata.push(5)
+
+console.log(stackdata)
+
+stackdata.pop()
+console.log(stackdata + 'remove')
+
+//Last in First out 
+
+//LIFO
