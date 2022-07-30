572f6618d4d38d176902ce234854a45b3acd9590
shakilmahammud <hshakil987@gmail.com>
2022-07-30 23:49:03 +0600

    cpp

--- loop/loop.cpp
+++ loop/loop.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     int n;
+//  cin>>n;
+//  int sum=0;
+//  for(int i=0;i<=n;i++){
+//     sum+=i;
+//  }
+//  cout<<sum<<endl;
+//     return 0;
+// }
+// all prime check 
+
 int main(){
-    int n;
- cin>>n;
- int sum=0;
- for(int i=0;i<=n;i++){
-    sum+=i;
- }
- cout<<sum<<endl;
-    return 0;
+   
 }
